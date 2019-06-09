360dff3e05d8a444a98e1eaf70cab6f158848270
Łukasz Starosta <lstarosta98@gmail.com>
2019-06-09 10:25:36 +0200

    Update part4_additional_Karczewski_Starosta.sql

--- part4_additional_Karczewski_Starosta.sql
+++ part4_additional_Karczewski_Starosta.sql
@@ -1,10 +1,37 @@
 /* A1. */
-SELECT last_name, job_id, salary FROM employees WHERE salary > (SELECT MAX(salary) FROM employees WHERE job_id = 'SA_MAN');
+--SELECT last_name, job_id, salary FROM employees WHERE salary > (SELECT MAX(salary) FROM employees WHERE job_id = 'SA_MAN');
 
 /* A2. */
-SELECT employee_id, last_name, Emp.department_id FROM employees Emp, departments Dep, locations Loc
-	WHERE Emp.department_id = Dep.department_id AND Dep.location_id = Loc.location_id AND SUBSTRING(Loc.city, 1, 1) = 'T';
+--SELECT employee_id, last_name, Emp.department_id FROM employees Emp, departments Dep, locations Loc
+--	WHERE Emp.department_id = Dep.department_id AND Dep.location_id = Loc.location_id AND SUBSTRING(Loc.city, 1, 1) = 'T';
 
-/* A3. possibly add a subquery in the FROM clause */
---SELECT last_name, salary, Emp.department_id, (SELECT AVG(salary) FROM employees, departments WHERE departments.department_id = employees.department_id) as Dept_AVG
---	FROM employees Emp, departments Dep WHERE Emp.
+/* A3. */
+--SELECT E1.last_name, E1.salary, E1.department_id, (SELECT avg(E2.salary) FROM employees E2 WHERE E1.department_id = E2.department_id) AS dept_avg
+--	FROM employees E1
+--	WHERE E1.salary > (SELECT avg(E2.salary) FROM employees E2 WHERE E1.department_id = E2.department_id)
+--	ORDER BY dept_avg, last_name ASC;
+
+/* A4. NOT EXISTS */
+--SELECT E1.last_name FROM employees E1 
+--	WHERE NOT EXISTS (SELECT * FROM employees E2 WHERE E1.employee_id = E2.manager_id);
+
+/* A4. NOT IN does not work */
+--SELECT E1.last_name FROM employees E1
+--	WHERE E1.employee_id NOT IN (SELECT E2.manager_id FROM employees E2);
+
+/* A5. */
+--SELECT E1.last_name
+--	FROM employees E1
+--	WHERE E1.salary < (SELECT avg(E2.salary) FROM employees E2 WHERE E1.department_id = E2.department_id);
+
+/* A6. */
+--SELECT DISTINCT E1.last_name, E1.employee_id
+--	FROM employees E1, employees E2
+--	WHERE E1.department_id = E2.department_id AND E1.salary < E2.salary AND  E1.hire_date < E2.hire_date
+--	ORDER BY E1.employee_id ASC;
+
+/* A7.  add the 1/8 thing */
+
+WITH SUMMARY (total_salary, department_id) AS
+	(SELECT SUM(salary), department_id FROM employees GROUP BY department_id)
+	SELECT department_name, total_salary FROM departments D, SUMMARY WHERE D.department_id = SUMMARY.department_id ;
